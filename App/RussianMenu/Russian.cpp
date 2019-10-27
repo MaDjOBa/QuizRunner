1d78474082375a0a588230c16aef696c4efbc04a
Daniil Vaidov <dorovokk@yandex.ru>
2019-10-28 05:14:51 +0700

    fix Russian

--- App/RussianMenu/Russian.cpp
+++ App/RussianMenu/Russian.cpp
@@ -4,27 +4,27 @@
 using namespace std;
 int Russian(int yes)
 {
-   system("cls");
+  
    yes = RQuestion1(yes);
-   system("cls");
+   
    yes = RQuestion2(yes);
-   system("cls");
+  
    yes = RQuestion3(yes);
-   system("cls");
+   
    yes = RQuestion4(yes);
-   system("cls");
+   
    yes = RQuestion5(yes);
-   system("cls");
+   
    yes = RQuestion6(yes);
-   system("cls");
+   
    yes = RQuestion7(yes);
-   system("cls");
+   
    yes = RQuestion8(yes);
-   system("cls");
+   
    yes = RQuestion9(yes);
-   system("cls");
+   
    yes = RQuestion10(yes);
-   system("cls");
+   
    return yes;
 }
 
