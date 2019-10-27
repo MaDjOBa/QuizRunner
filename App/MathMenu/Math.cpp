bb415d4478d49d7030e9653e145e8f181dde2dd3
Daniil Vaidov <dorovokk@yandex.ru>
2019-10-28 04:28:02 +0700

    Math menu pull

--- App/MathMenu/Math.cpp
+++ App/MathMenu/Math.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include "functions.h"
+using namespace std;
+int Math(int yes)
+{
+   system("cls");
+   yes = MQuestion1(yes);
+   system("cls");
+   yes = MQuestion2(yes);
+   system("cls");
+   yes = MQuestion3(yes);
+   system("cls");
+   yes = MQuestion4(yes);
+   system("cls");
+   yes = MQuestion5(yes);
+   system("cls");
+   yes = MQuestion6(yes);
+   system("cls");
+   return yes;
+}
 
