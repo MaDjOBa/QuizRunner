b08a4abe49511c33f7bf3145c93725ce7a7aa388
Daniil Vaidov <dorovokk@yandex.ru>
2019-10-28 05:17:09 +0700

    fix Math.cpp

--- App/MathMenu/Math.cpp
+++ App/MathMenu/Math.cpp
@@ -3,19 +3,19 @@
 using namespace std;
 int Math(int yes)
 {
-   system("cls");
+   
    yes = MQuestion1(yes);
-   system("cls");
+   
    yes = MQuestion2(yes);
-   system("cls");
+   
    yes = MQuestion3(yes);
-   system("cls");
+   
    yes = MQuestion4(yes);
-   system("cls");
+   
    yes = MQuestion5(yes);
-   system("cls");
+   
    yes = MQuestion6(yes);
-   system("cls");
+   
    return yes;
 }
 
